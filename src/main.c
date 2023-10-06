7e6b8ebfa1158fc97b25f939350e66c5788b006c
Warre Devriese <wdevries@a1-r4-p1.s19.be>
2023-10-06 17:22:08 +0200

    Refactor complete

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 13:35:54 by wdevries          #+#    #+#             */
-/*   Updated: 2023/10/06 17:06:43 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/10/06 17:21:34 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,13 @@ int     ft_openMapFile(const char *mapFile)
         write(1, "System error\n", 13);
     return (fd);
 }
-void    ft_fdf(const char *mapFile)
+
+static void ft_displayMap(t_pointCoordinates **mapCoordinates)    
+{
+
+}
+
+static void ft_createMap(const char *mapFile)
 {
     t_mapInfo           mapInfo;
     t_pointCoordinates  **mapCoordinates;
@@ -40,7 +46,7 @@ int     main(int argc, char **argv)
     if (argc != 2)
         write(1, "Usage: ./fdf <map>\n", 19);
     else
-        ft_fdf((const char*)argv[1]);
+        ft_createMap((const char*)argv[1]);
     return (0);
 }
 
